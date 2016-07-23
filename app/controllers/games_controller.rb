35cee44917ba737c6735df75cf2af7053bf83f23
David OKeefe <davidpokeefe3@gmail.com>
2016-07-23 15:29:17 -0400

    Add ability to edit game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,20 @@ class GamesController < ApplicationController
     redirect_to game_players_path(game_id: game.id)
   end
 
+  def edit
+    @game = Game.find(params[:id])
+  end
+
+  def update
+    game = Game.find(params[:id])
+    if game.update_attributes(game_params)
+      flash[:notice] = 'Game Updated'
+      redirect_to game_players_path(game_id: game.id)
+    else
+      flash[:notice] = 'Something went wrong'
+    end
+  end
+
   private
   def game_params
     params.require(:game).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   root to: "games#new"
 
-  resources :games, only: [:new, :create] do
+  resources :games, only: [:new, :create, :edit, :update] do
     resources :players, only: [:index, :new, :create, :edit, :update]
     resource :scorecard, only: [:show, :create]
   end
