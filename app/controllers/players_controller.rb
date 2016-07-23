2bb0a65549a6daacb6684965e6846754af70437f
David OKeefe <davidpokeefe3@gmail.com>
2016-07-23 15:21:38 -0400

    Add ability to update player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -15,6 +15,21 @@ class PlayersController < ApplicationController
     redirect_to game_players_path(game_id: params[:game_id])
   end
 
+  def edit
+    @game = Game.find(params[:game_id])
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    player = Player.find(params[:id])
+    if player.update_attributes(player_params)
+      flash[:notice] = "Player Updated"
+      redirect_to game_players_path(game_id: params[:game_id])
+    else
+      flash[:alert] = "Problem Saving Player"
+    end
+  end
+
   private
   def player_params
     params.require(:player).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "games#new"
 
   resources :games, only: [:new, :create] do
-    resources :players, only: [:index, :new, :create]
+    resources :players, only: [:index, :new, :create, :edit, :update]
     resource :scorecard, only: [:show, :create]
   end
 end
